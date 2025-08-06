751d4d0d65d7e73e5a74f35e1182f1ae2efa7a69
greenhc502 <greenhc502@gmail.com>
2025-08-06 17:26:57 +0900

    그린 그램 - 회원 가입 예외처리

--- src/main/java/com/green/greengram/application/user/model/UserSignUpReq.java
+++ src/main/java/com/green/greengram/application/user/model/UserSignUpReq.java
@@ -9,12 +9,12 @@ import lombok.ToString;
 @Getter
 // 이게validation
 public class UserSignUpReq {
-    @Size(max = 50,min= 4, message = "아이디는 4~50자 까지 작성 할 수 있습니다.")// uid 최대 최소 글자수 설정
-    @Pattern(regexp = "[A-Za-z0-9]_{4,50}", message = "아이디는 영어,숫자, 언더바로만 작성가능하고 4자이상 50이하 까지만 가능합니다.")
+    @Pattern(regexp = "^[A-Za-z0-9_]{4,50}$", message = "아이디는 영어, 숫자, 언더바로만 구성되어야 하며 4~50자까지 작성할 수 있습니다.")
     private String uid;
 
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).{10,}$", message = "비밀번호는 대소문자, 숫자, 특수문자를 포함하여 10자 이상이어야 합니다." )
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).{10,}$", message = "비밀번호는 영문자, 숫자, 특수기호로 구성되며 10자 이상이어야 합니다.")
     private String upw;
-    @Pattern(regexp ="^[가-힣]{2,10}$", message="닏네임은 한글로 2~10자 까지 가능합니다." )
+
+    @Pattern(regexp = "^[가-힣]{2,10}$", message = "닉네임은 한글로 2~10자까지 가능합니다.")
     private String nickName;
 }
