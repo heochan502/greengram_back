94a554c5529c9801ce838f474e0e101dbfbb8e8e
greenhc502 <greenhc502@gmail.com>
2025-08-07 11:41:23 +0900

    그린 그램 - 이미지 입력 까지 하는거

--- src/main/java/com/green/greengram/application/user/UserService.java
+++ src/main/java/com/green/greengram/application/user/UserService.java
@@ -2,11 +2,13 @@ package com.green.greengram.application.user;
 
 
 import com.green.greengram.application.user.model.UserSignUpReq;
+import com.green.greengram.config.util.ImgUploadManager;
 import com.green.greengram.entity.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
@@ -15,7 +17,9 @@ import org.springframework.web.multipart.MultipartFile;
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final ImgUploadManager imgUploadManager;
 
+    @Transactional // 트랜젝션 기능
     public void signup(UserSignUpReq req, MultipartFile pic)
     {
         String hashedPassword = passwordEncoder.encode(req.getUpw());
@@ -28,6 +32,12 @@ public class UserService {
         user.addUserRole(req.getRoles());
 
         userRepository.save(user);
+
+        if(pic != null) {
+            String savedFileName = imgUploadManager.saveProfilePic(user.getUserId(), pic);
+            user.setPic(savedFileName);
+//            throw new RuntimeException(); // 강제로 예외 발생시켜서 트랜젝션 테스트용
+        }
     }
 
 }
